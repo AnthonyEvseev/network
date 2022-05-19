63a595698ce562f27d59f94d1d3ecb0272c88f1c
Anthony <tosharus26@gmail.com>
2022-05-19 15:17:11 +0300

    Дописал модели

--- social_network/app_posts/models.py
+++ social_network/app_posts/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class PostClass(models.Model):
     Post_id = models.IntegerField()
-    Title_post_tr = models.CharField()
-    Title_post = models.CharField()
-    Author = models.CharField()
-    Crop_content = models.CharField(250)
-    Full_content = models.CharField()
+    Title_post_tr = models.CharField(max_length=50)
+    Title_post = models.CharField(max_length=50)
+    Author = models.CharField(max_length=50)
+    Crop_content = models.CharField(max_length=250)
+    Full_content = models.CharField(max_length=50)
     Views_post = models.IntegerField()
     Favorites = models.BooleanField()
     Likes = models.SmallIntegerField(-1, 0, 1)
--- social_network/app_users/models.py
+++ social_network/app_users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class UserClass(models.Model):
-    user_name = models.CharField(max_length=50)
-    login = models.CharField()
-    password = models.CharField()
-    email = models.CharField()
+    user_name = models.CharField(max_length=30)
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
