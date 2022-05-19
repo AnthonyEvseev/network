9c3ac7049459c7b41d57b60544a37344ff16b67e
anthony <tosharus26@gmail.com>
2022-05-19 17:49:14 +0300

    Подключил Rest

--- social_network/app_posts/views.py
+++ social_network/app_posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import PostClass
+from .serializer import PostSerializer
 
-# Create your views here.
 
 class PostAPIView(generics.ListAPIView):
     queryset = PostClass.object.all()
